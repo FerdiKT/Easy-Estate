536cf242d497805f77b69c8cf254c34648b349af
Fatih Barmanbay <fthbrmnby@gmail.com>
2015-12-12 22:37:20 +0200

    Calling and opening web page operations added

--- app/src/main/java/com/milyonbit/easyestate/Result.java
+++ app/src/main/java/com/milyonbit/easyestate/Result.java
@@ -1,9 +1,11 @@
 package com.milyonbit.easyestate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -19,13 +21,13 @@ public class Result extends AppCompatActivity {
         setContentView(R.layout.result);
 
         TextView resultOwnerName = (TextView) findViewById(R.id.result_owner_name);
-        TextView resultPhone = (TextView) findViewById(R.id.result_phone);
+        final TextView resultPhone = (TextView) findViewById(R.id.result_phone);
         TextView resultType = (TextView) findViewById(R.id.result_type);
         TextView resultPrice = (TextView) findViewById(R.id.result_price);
         TextView resultSize = (TextView) findViewById(R.id.result_size);
         TextView resultRoomCount = (TextView) findViewById(R.id.result_room_count);
         TextView resultCredit = (TextView) findViewById(R.id.result_credit);
-        TextView resultLink = (TextView) findViewById(R.id.result_link);
+        final TextView resultLink = (TextView) findViewById(R.id.result_link);
         TextView resultDesc = (TextView) findViewById(R.id.result_description);
 
 
@@ -46,5 +48,43 @@ public class Result extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        resultPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                call(resultPhone.getText().toString());
+            }
+        });
+
+        resultLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebURL(resultLink.getText().toString());
+            }
+        });
+    }
+
+    public void call(String number) {
+        try {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + number));
+            startActivity(callIntent);
+        } catch (SecurityException e) {
+            Log.e("HATA", e.toString());
+        }
+    }
+
+    public void openWebURL( String url ) {
+
+        if(!url.startsWith("http://") || !url.startsWith("https://"))
+            url = "http://" + url;
+        try{
+            Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse( url ) );
+            startActivity( browse );
+        }
+        catch (Exception e){
+            Log.e("ERROR ON WEB PAGE",e.toString());
+        }
+
     }
 }
